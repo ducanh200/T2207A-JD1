ba36b0e8ccfd9998b2a3a095c3b313e2882c45e5
ducanh2004 <110523685+ducanh2004@users.noreply.github.com>
2023-02-08 15:40:46 +0700

    bai thi

---
+++ src/baithi/BookDemo.java
@@ -0,0 +1,10 @@
+package baithi;
+
+public class BookDemo {
+    public static void main(String []args){
+        Author author = new Author("Russel", "Winderand");
+        Book book = new Book("Developing Java Software","Russel Winderand", 79.75);
+        System.out.println(book);
+    }
+}
+
